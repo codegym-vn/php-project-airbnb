1e365167f9c021b245c40b88aacbc7ceae45a6c7
LinhC0918 <nguyenvanlinh12d3@gmail.com>
2019-01-09 15:59:10 +0700

    role error

--- resources/views/house/list.blade.php
+++ resources/views/house/list.blade.php
@@ -1,46 +1,49 @@
 @extends('house.layout')
 
 @section('content')
-    <div class="col-12">
-        <div class="row">
-            <div class="col-12"><h1>My House</h1></div>
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Content</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Address</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Image</th>
-                </tr>
-                </thead>
-                @if(count($houses) == 0)
+    <div class="container">
+        <div class="col-12">
+            <div class="row">
+                <div class="col-12"><h1 style="text-align: center">My House</h1></div>
+                <table class="table table-striped">
+                    <thead>
                     <tr>
-                        <td colspan="4">Không có dữ liệu</td>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Content</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Image</th>
                     </tr>
-                @else
-                    @foreach($houses as $key => $house)
+                    </thead>
+                    @if(count($houses) == 0)
                         <tr>
-                            <th scope="row"{{++$key}}></th>
-                            <td>{{$house->title}}</td>
-                            <td>{{$house->description}}</td>
-                            <td>{{$house->content}}</td>
-                            <td>{{$house->price}}</td>
-                            <td>{{$house->address}}</td>
-                            <td>{{$house->status}}</td>
-                            <td><img src="{{asset('storage/' . $house->image)}}" style="height:200px; width:200px"></td>
-
-                            <td><a class="btn btn-success" href="{{ route('house.edit', $house->id) }}">sửa</a></td>
-                            <td><a class="btn btn-danger" href="{{ route('house.destroy', $house->id) }}"
-                                   onclick="return confirm('Bạn chắc chắn muốn xóa?')">xóa</a></td>
+                            <td colspan="4">Không có dữ liệu</td>
                         </tr>
-                    @endforeach
-                @endif
-            </table>
-            <a class="btn btn-primary" href="{{ route('house.create') }}">Thêm mới</a>
+                    @else
+                        @foreach($houses as $key => $house)
+                            <tr>
+                                <th scope="row"{{++$key}}></th>
+                                <td>{{$house->title}}</td>
+                                <td>{{$house->description}}</td>
+                                <td>{{$house->content}}</td>
+                                <td>{{$house->price}}</td>
+                                <td>{{$house->address}}</td>
+                                <td>{{$house->status}}</td>
+                                <td><img src="{{asset('storage/' . $house->image)}}" style="height:200px; width:200px">
+                                </td>
+
+                                <td><a class="btn btn-success" href="{{ route('house.edit', $house->id) }}">sửa</a></td>
+                                <td><a class="btn btn-danger" href="{{ route('house.destroy', $house->id) }}"
+                                       onclick="return confirm('Bạn chắc chắn muốn xóa?')">xóa</a></td>
+                            </tr>
+                        @endforeach
+                    @endif
+                </table>
+                <a class="btn btn-primary" href="{{ route('house.create') }}">Thêm mới</a>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,56 +39,9 @@
         </div>
 
     </nav>
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100"
-                     src="https://cdn.luxstay.com/rooms/12043/large/1518430184_Phong_kh%C3%A1ch_(Toan_canh).jpg"
-                     alt="First slide" style="height: 400px; width: 300px">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="https://cdn.luxstay.com/rooms/15176/large/1537408618_IMG_7906.jpg"
-                     alt="Second slide" style="height: 400px; width: 300px">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100"
-                     src="https://cdn.luxstay.com/rooms/14436/large/1534908497_image_6483441%20(11).JPG"
-                     alt="Third slide" style="height: 400px; width: 300px">
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-    <div class="row">
-        @foreach($houses as $key=>$house)
 
-            <div class="col-md-4">
-                <img class="card-img-top" src="https://cdn.luxstay.com/rooms/11930/large/1515401896_DSC_0516.jpg" alt=""
-                     style="width: 350px; height: 200px">
-                <div class="card-body">
-                    <p>{{$house->title}}</p>
-                    <p>{{$house->price . ' đ'}}</p>
-                </div>
-            </div>
-        @endforeach
-    </div>
-    <textarea class="form-control" id="summary-ckeditor"></textarea>
 </div>
-<script src="{{ asset('vendor/unisharp/laravel-ckeditor/ckeditor.js') }}"></script>
-<script>
-    CKEDITOR.replace('summary-ckeditor');
-</script>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
