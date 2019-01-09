af21c011996931d3615719f6a49481938487b180
manhsanhcong <manhsanhcong.08@gmail.com>
2019-01-09 14:35:00 +0700

    crud-admin

---
+++ resources/views/house/edit.blade.php
@@ -0,0 +1,60 @@
+@extends('house.layout')
+
+@section('content')
+    <div class="col-12 col-md-12">
+        <div class="row">
+            <div class="col-12">
+                <h1>Cập nhật House</h1>
+            </div>
+            <div class="col-12">
+                <form method="post" action="{{route('house.update', $house->id)}}" enctype="multipart/form-data">
+                    @csrf
+                    <input type="hidden" name="id" value="{{$house->id}}"/>
+                    <div class="form-group">
+                        <label>Title</label>
+                        <input type="text" name="title" value="{{$house->title}}"
+                               class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Description</label>
+                        <textarea name="description" class="form-control">{{$house->description}}</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Content</label>
+                        <textarea name="content" class="form-control">{{$house->content}}</textarea>
+                        @if($errors->has('content'))
+                            <p class="help is-danger" style="color: red;">{{ $errors->first('content') }}</p>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label>Price</label>
+                        <input type="text" name="price" value="{{$house->price}}"class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Address</label>
+                        <input type="text" name="address" value="{{$house->address}}" class="form-control"/>
+                        @if($errors->has('address'))
+                            <p class="help is-danger" style="color: red;">{{ $errors->first('address') }}</p>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label>Status</label>
+                        <input type="text" name="status" value="{{$house->status}}" class="form-control"/>
+                        @if($errors->has('status'))
+                            <p class="help is-danger" style="color: red;">{{ $errors->first('status') }}</p>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label>Image</label>
+                        <input type="file" name="image" value="{{$house->image}}"class="form-control-file"/>
+                        @if($errors->has('image'))
+                            <p class="help is-danger" style="color: red;">{{ $errors->first('image') }}</p>
+                        @endif
+                    </div>
+                    <button type="submit" class="btn btn-primary">Cập nhật</button>
+                    <button class="btn btn-secondary" onclick="window.history.go(-1); return false;">Hủy</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/house/list.blade.php
@@ -0,0 +1,47 @@
+@extends('house.layout')
+
+@section('content')
+    <div class="col-12">
+        <div class="row">
+            <div class="col-12"><h1>My House</h1></div>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Content</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Image</th>
+                </tr>
+                </thead>
+                @if(count($houses) == 0)
+                    <tr>
+                        <td colspan="4">Không có dữ liệu</td>
+                    </tr>
+                @else
+                    @foreach($houses as $key => $house)
+                        <tr>
+                            <th scope="row"{{++$key}}></th>
+                            <td>{{$house->title}}</td>
+                            <td>{{$house->description}}</td>
+                            <td>{{$house->content}}</td>
+                            <td>{{$house->price}}</td>
+                            <td>{{$house->address}}</td>
+                            <td>{{$house->status}}</td>
+                            <td><img src="{{'/upload/images/' . $house->image}}" style="height:200px" , width=200px></td>
+
+                            <td><a class="btn btn-success" href="{{ route('house.edit', $house->id) }}">sửa</a></td>
+                            <td><a class="btn btn-danger" href="{{ route('house.destroy', $house->id) }}"
+                                   class="text-danger"
+                                   onclick="return confirm('Bạn chắc chắn muốn xóa?')">xóa</a></td>
+                        </tr>
+                    @endforeach
+                @endif
+            </table>
+            <a class="btn btn-primary" href="{{ route('house.create') }}">Thêm mới</a>
+        </div>
+    </div>
+@endsection
